17605524edf92130fa654364bca9fc3f85a2d38b
FISIU <fishbone_1988@o2.pl>
2018-08-28 01:27:38 +0200

    VER 1

---
+++ AutoRunWB/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace AutoRunWB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.WebBrowser = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.url = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.run = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(427, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "RUN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.WebBrowser,
+            this.url,
+            this.run});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 0);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(763, 342);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // WebBrowser
+            // 
+            this.WebBrowser.FillWeight = 30F;
+            this.WebBrowser.HeaderText = "WebBrowser";
+            this.WebBrowser.Items.AddRange(new object[] {
+            "Chrome",
+            "Internet Explorer"});
+            this.WebBrowser.Name = "WebBrowser";
+            // 
+            // url
+            // 
+            this.url.HeaderText = "url";
+            this.url.Name = "url";
+            // 
+            // run
+            // 
+            this.run.FillWeight = 10F;
+            this.run.HeaderText = "run";
+            this.run.Name = "run";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(657, 346);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "DODAJ URL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(542, 346);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 28);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "ZAPISZ DANE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(115, 352);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(164, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Zamknij procesy Chrome";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(287, 352);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(134, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Zamknij procesy IE";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(-13, 348);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 28);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "RUN";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(768, 377);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AutoRunWB by Fisiu [1.00.00]";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewComboBoxColumn WebBrowser;
+        private System.Windows.Forms.DataGridViewTextBoxColumn url;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn run;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ AutoRunWB/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+
+namespace AutoRunWB
+{
+    public partial class Form1 : Form
+    {
+        public class cfg
+        {
+            public string browser { get; set; }
+            public string url_ { get; set; }
+            public string run_ { get; set; }
+        }
+
+        public List<cfg> setting = new List<cfg>();
+        public List<string> IE_url = new List<string>();
+        public List<string> Chrome_url = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            LoadConfig();
+            foreach (cfg xy in setting)
+            {
+                if (xy.run_ == "F")
+                {
+                    dataGridView1.Rows.Add(null, xy.url_, false);
+                }
+                else
+                {
+                    dataGridView1.Rows.Add(null, xy.url_, true);
+                }
+                if (xy.browser == "Chrome")
+                {
+                    dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Value = "Chrome";
+                }
+                else
+                {
+                    dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Value = "Internet Explorer";
+                }
+             }     
+        }
+
+
+
+
+        //------------------------------------------------------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //------------------------------------------------------------------------------------------------------
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IE_url.Clear();
+            Chrome_url.Clear();
+            //IE
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if ((Convert.ToBoolean(row.Cells[2].Value) == true) && (Convert.ToString(row.Cells[0].Value) == "Internet Explorer"))
+                {
+                    IE_url.Add(Convert.ToString(row.Cells[1].Value));
+                }
+            }
+
+            //Chrome
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if ((Convert.ToBoolean(row.Cells[2].Value) == true) && (Convert.ToString(row.Cells[0].Value) == "Chrome"))
+                {
+
+                   Chrome_url.Add(Convert.ToString(row.Cells[1].Value));
+                }
+            }
+
+            //WYKONYWANIE
+            //IE
+            char znaczek = (char)39;
+            int i = 0;
+
+            string skrypt = "";
+
+         
+            foreach (string x in IE_url)
+            {
+                if (i > 0)
+                {
+                    skrypt = skrypt + "$ie.Navigate2(" + znaczek + x + znaczek + ", 0x1000)  " + "\r\n";
+                }
+                else
+                {
+                    skrypt = skrypt + "$ie.Navigate2(" + znaczek + x + znaczek + ") " + "\r\n";
+                }
+                i++;
+            }
+            if (i > 0)
+            {
+                RunScript("$ie = New-Object -ComObject InternetExplorer.Application \r\n" + skrypt + "\r\n $ie.Visible = $true");
+            }
+
+            skrypt = "";
+            i = 0;
+            foreach (string x in Chrome_url)
+            {
+                //"google.com" + " --new-window " + "google.com" + " --new-window --incognito";
+                skrypt = skrypt + x + " --new-window ";
+                i++;
+            }
+            if (i > 0)
+            {
+                Process process = new Process();
+                string chromeAppFileName = ChromeAppFileName;
+                if (string.IsNullOrEmpty(chromeAppFileName))
+                {
+                    throw new Exception("Could not find chrome.exe!");
+                }
+
+                process.StartInfo.FileName = chromeAppFileName; // @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+                process.StartInfo.Arguments = skrypt;//"google.com" + " --new-window " + "google.com" + " --new-window --incognito";
+                process.Start();
+            }
+
+        }
+        //------------------------------------------------------------------------------------------------------
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(null, "", true);         
+            dataGridView1.Rows[dataGridView1.Rows.Count-1].Cells[0].Value = "Internet Explorer";
+        }
+
+        //------------------------------------------------------------------------------------------------------
+        private string RunScript(string scriptText)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            try
+            {
+                Runspace runspace = RunspaceFactory.CreateRunspace();
+
+                runspace.Open();
+                Pipeline pipeline = runspace.CreatePipeline();
+                pipeline.Commands.AddScript(scriptText);
+                pipeline.Commands.Add("Out-String");
+
+                Collection<PSObject> results = pipeline.Invoke();
+                runspace.Close();
+             
+                foreach (PSObject obj in results)
+                {
+                    stringBuilder.AppendLine(obj.ToString());
+                }
+            }
+            catch
+            { }
+
+            return stringBuilder.ToString();
+        }
+
+
+
+        private const string ChromeAppKey = @"\Software\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe";
+
+        private static string ChromeAppFileName
+        {
+            get
+            {
+                return (string)(Registry.GetValue("HKEY_LOCAL_MACHINE" + ChromeAppKey, "", null) ??
+                                Registry.GetValue("HKEY_CURRENT_USER" + ChromeAppKey, "", null));
+            }
+        }
+
+        public static void OpenLink(string url)
+        {
+            string chromeAppFileName = ChromeAppFileName;
+            if (string.IsNullOrEmpty(chromeAppFileName))
+            {
+                throw new Exception("Could not find chrome.exe!");
+            }
+            Process.Start(chromeAppFileName, url);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //  OpenLink("www.google.com --newwindow");
+            string aktualny_cfg = "";
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[2].Value.ToString() == "True")
+                {
+                    aktualny_cfg += row.Cells[0].Value.ToString() + "<M>" + row.Cells[1].Value.ToString() + "<M>" + "T" + "\r\n";
+                }
+                else
+                {
+                    aktualny_cfg += row.Cells[0].Value.ToString() + "<M>" + row.Cells[1].Value.ToString() + "<M>" + "F" + "\r\n";
+                }
+            }
+            System.IO.File.WriteAllText("config.cfg", aktualny_cfg);
+
+            //Process process = new Process();
+            // string chromeAppFileName = ChromeAppFileName;
+            // if (string.IsNullOrEmpty(chromeAppFileName))
+            // {
+            //     throw new Exception("Could not find chrome.exe!");
+            // }
+
+            // process.StartInfo.FileName = chromeAppFileName; // @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+            // process.StartInfo.Arguments = "google.com" + " --new-window " + "google.com" + " --new-window --incognito";
+            // process.Start();
+        }
+
+
+        public void LoadConfig()
+        {
+            try
+            {
+                string[] stringSeparators = new string[] { "<M>" };
+                string[] lines = System.IO.File.ReadAllLines("config.cfg");
+                setting.Clear();
+                int kolejny = 0;
+                int licz = 0;
+                string result = "";
+                foreach (string line in lines)//ŁADOWANIE NAGŁÓWKA
+                {
+                    string[] xxx = line.Split(stringSeparators, StringSplitOptions.None);
+
+                    setting.Add(new cfg()
+                    {
+                        browser = xxx[0],
+                        url_ = xxx[1],
+                        run_ = xxx[2]
+                    });
+                }
+            }
+            catch
+            { }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+            try
+            {
+                foreach (var process in processes)
+                {
+                    if (process.ProcessName == "chrome")
+                    {
+                        process.Kill();
+                    }
+                }
+            }
+            catch 
+            { }
+        
+       
+        }
+    }
+}
